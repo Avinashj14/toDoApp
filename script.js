35e8b8dd410da6b8e4c06c3cf529625980323443
Avinash <javinash140@gmail.com>
2023-09-06 02:03:46 +0530

    updating our js

--- script.js
+++ script.js
@@ -1,268 +1,165 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const inputBox = document.getElementById("input-box");
-    const todoList = document.getElementById("todo-list");
-  
+let todos = [];
+
+function addTask() {
+    const taskInput = document.getElementById("taskInput");
+    const taskText = taskInput.value.trim();
+
+    if (taskText === "") {
+        alert("Please enter a task.");
+        return;
+    }
+
+    const todolist = document.getElementById("todolist");
+    const taskDiv = document.createElement("div");
+    taskDiv.className = "listContent";
+
+    const checkboxDiv = document.createElement("div");
+    checkboxDiv.className = "checkbox";
+    checkboxDiv.innerHTML = '<div class="check"><img src="./assets/icon-check.svg" /></div>';
+
+    const textDiv = document.createElement("div");
+    textDiv.className = "text";
+    textDiv.innerHTML = `<p>${taskText}</p>`;
+
+    const delDiv = document.createElement("div");
+    delDiv.className = "del";
+    delDiv.innerHTML = '<img src="./assets/icon-cross.svg" />';
+
+    taskDiv.appendChild(checkboxDiv);
+    taskDiv.appendChild(textDiv);
+    taskDiv.appendChild(delDiv);
+
+    todolist.appendChild(taskDiv);
 
 
+    checkboxDiv.addEventListener("click", () => completeTask(taskDiv));
+    delDiv.addEventListener("click", () => deleteTask(taskDiv));
+
+    todos.push({ text: taskText, completed: false });
+
+    taskInput.value = "";
+    updateTaskCount();
+}
+
+function completeTask(task) {
+    task.classList.toggle("completed");
+
+    const span = task.querySelector("p");
+
+    const taskText = span.textContent;
+    const index = todos.findIndex(todo => todo.text === taskText);
+    if (index !== -1) {
+        todos[index].completed = !todos[index].completed;
+
+        if (todos[index].completed) {
+            span.style.textDecoration = "line-through";
+        } else {
+            span.style.textDecoration = "none";
+        }
+    }
+
+    updateTaskCount();
+}
+
+function deleteTask(task) {
+    const todolist = document.getElementById("todolist");
+    const span = task.querySelector("p");
+    const taskText = span.textContent;
 
 
+    const index = todos.findIndex(todo => todo.text === taskText);
+    if (index !== -1) {
+        todos.splice(index, 1);
+    }
 
 
-    const todoItems = []; 
-  
-    function displayitem() {
-      todoList.innerHTML = todoItems
-        .map((item) => {
-          return `
-            <div class="listContent">
-              <div class="checkbox">
-                <div class="check">
-                  <img src="./assets/icon-check.svg" />
-                </div>
-              </div>
-              <div class="text">
-                <p>${item}</p>
-              </div>
-              <div class="del">
-              <img src="./assets/icon-cross.svg" />
-              </div>
-            </div>
-          `;
-        })
-        .join("");
+    todolist.removeChild(task);
+
+    updateTaskCount();
+}
+
+function filterTasks(filter) {
+    const todolist = document.getElementById("todolist");
+    const tasks = todolist.getElementsByClassName("listContent");
+
+    for (const task of tasks) {
+        const isCompleted = task.classList.contains("completed");
+        if (filter === "all" || (filter === "active" && !isCompleted) || (filter === "completed" && isCompleted)) {
+            task.style.display = "flex";
+        } else {
+            task.style.display = "none";
+        }
+    }
 
 
-        const items = document.querySelectorAll(".listContent");
-        items.forEach((item, index) => {
+    const filterButtons = document.querySelectorAll(".filter");
+    filterButtons.forEach(button => button.classList.remove("active"));
+    document.getElementById(filter + "Btn").classList.add("active");
 
-            //checkbox, delete button, and text div
-          const checkbox = item.querySelector(".checkbox");
-          const delButton = item.querySelector(".del");
-          const textDiv = item.querySelector(".text");
-    
-          //check
-          checkbox.addEventListener("click", function () {
-            const checkboxImg = checkbox.querySelector("img");
-            checkboxImg.classList.toggle("visible");
-    
-            // Apply line-through effect to the paragraph text
-            if (textDiv) {
-              const textParagraph = textDiv.querySelector("p");
-              textParagraph.classList.toggle("completed");
-            }
-          });
-   
-   
-          //del
-          delButton.addEventListener("click", function () {
-            item.remove();
-            todoItems.splice(index, 1);
-            displayitem();
-          });
-        });
+    updateTaskCount();
+}
+
+function clearCompleted() {
+    const todolist = document.getElementById("todolist");
+    const completedTasks = todolist.getElementsByClassName("completed");
+
+    while (completedTasks.length > 0) {
+        todolist.removeChild(completedTasks[0]);
+    }
 
 
+    todos = todos.filter(todo => !todo.completed);
+    updateTaskCount();
+}
 
+function updateTaskCount() {
+    const itemsLeft = document.getElementById("itemsleft");
+    const totalTasks = todos.length;
+    const incompleteTasks = todos.filter(todo => !todo.completed).length;
+    itemsLeft.textContent = `Task (${incompleteTasks})`;
+}
 
-        const sunIcon = document.querySelector(".content img");
-        const backgroundImg = document.querySelector(".backgroundImg img");
-        const body = document.body;
-     
-  sunIcon.addEventListener("click", function () {
-   
-    const currentSrc = sunIcon.getAttribute("src");
+function toggleTheme() {
+    const body = document.body;
+    const themeToggle = document.getElementById("themeToggle");
+    const backgroundImage = document.querySelector(".backgroundImg img");
+    const listContents = document.querySelectorAll(".listContent");
+    const addButton = document.querySelector(".newTodo button");
+    const todoInfo = document.querySelector(".todoInfo");
+    body.classList.toggle("dark-mode");
+    body.classList.toggle("light-mode");
 
-   
-    if (currentSrc === "./assets/icon-sun.svg") {
-      // Change the src attribute to moon icon
-      sunIcon.setAttribute("src", "./assets/icon-moon.svg");
-      backgroundImg.setAttribute("src", "./assets/bg-desktop-light.jpg");
-      body.style.backgroundColor = "white"; 
+    const tasks = document.querySelectorAll(".task");
+    for (const task of tasks) {
+        task.classList.toggle("dark-mode");
+        task.classList.toggle("light-mode");
+    }
+
+    const taskInput = document.getElementById("taskInput");
+    taskInput.classList.toggle("dark-mode");
+    taskInput.classList.toggle("light-mode");
+
+    for (const listContent of listContents) {
+        listContent.classList.toggle("light-mode");
+        listContent.classList.toggle("dark-mode");
+        listContent.style.backgroundColor = body.classList.contains("light-mode") ? "white" : "rgb(45, 51, 45)";
+        listContent.style.color = body.classList.contains("light-mode") ? "rgb(45, 51, 45)" : "white";
+    }
+
+    if (body.classList.contains("dark-mode")) {
+        themeToggle.src = "./assets/icon-sun.svg";
+        backgroundImage.src = "./assets/bg-desktop-dark.jpg";
+        addButton.style.backgroundColor = "#333";
+        addButton.style.color = "white";
+        todoInfo.style.backgroundColor = "rgb(45, 51, 45)";
+        todoInfo.style.border = "1px solid rgb(67 67 67)";
     } else {
-      
-      sunIcon.setAttribute("src", "./assets/icon-sun.svg");
-      backgroundImg.setAttribute("src", "./assets/bg-desktop-dark.jpg");
-      body.style.backgroundColor = "black"; }
-  });
-      
-
-
-      }
-    
-  
-    displayitem();
-  
-
-    // event to add adding item 
-    inputBox.addEventListener("keydown", function (event) {
-      if (event.key === "Enter" && inputBox.value !== "") {
-        event.preventDefault(); 
-  
-        const newItemText = inputBox.value;
-        todoItems.push(newItemText);
-  
-        displayitem();
-  
-        inputBox.value = "";
-      }
-    });
-  });
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        themeToggle.src = "./assets/icon-moon.svg";
+        backgroundImage.src = "./assets/bg-desktop-light.jpg";
+        addButton.style.backgroundColor = "white";
+        addButton.style.color = "#333";
+        todoInfo.style.backgroundColor = "white";
+        todoInfo.style.border = "1px solid rgb(219, 218, 218)";
+    }
+}
 
